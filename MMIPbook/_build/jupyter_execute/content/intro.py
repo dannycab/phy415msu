5d88103dff981ad030c406f7b4d5158046806512
Danny Caballero <danny.caballero@gmail.com>
2022-03-06 10:11:42 -0500

    edits to SHO model

--- MMIPbook/_build/jupyter_execute/content/intro.py
+++ MMIPbook/_build/jupyter_execute/content/intro.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# ![assets/tc_big.jpg](../assets/images/tc_big.jpg)
+# # Welcome to Mathematical Modeling in Physics - Under Construction
 # 
-# # Welcome to Mathematical Modeling in Physics
+# <!-- ![assets/tc_big.jpg](../assets/images/tc_big.jpg) -->
+# <iframe src="https://giphy.com/embed/ZTans30ONaaIM" width="480" height="299" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/ZTans30ONaaIM">via GIPHY</a></p>
+# 
 # 
 # In this course, you will learn to:
 # 

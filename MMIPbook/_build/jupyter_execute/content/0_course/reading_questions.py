589f730fd36579ac6de2e5107884665a830eeead
dannycab <danny.caballero@pm.me>
2022-08-30 13:00:01 -0400

    updated web version again

---
+++ MMIPbook/_build/jupyter_execute/content/0_course/reading_questions.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Reading Questions Form
+# <iframe src="https://forms.office.com/r/jvHGdfyt64" style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="yes"></iframe>

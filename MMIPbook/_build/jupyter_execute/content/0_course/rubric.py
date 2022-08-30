0fbca19d5ce862d703c90166e8bd300e1d2a0bf2
dannycab <danny.caballero@pm.me>
2022-08-30 12:43:18 -0400

    built without notebooks to push to web

--- MMIPbook/_build/jupyter_execute/content/0_course/rubric.py
+++ MMIPbook/_build/jupyter_execute/content/0_course/rubric.py
@@ -4,3 +4,15 @@
 # # Project Rubrics
 # 
 # ## Preliminary (For first out of class project)
+# 
+# Our rubric will initially focus on the [core learning goals](goals.md). As we develop expertise in these areas, we might decide to change emphases or focus.
+# 
+# | Goal                                                      | Fractional Importance |
+# |-----------------------------------------------------------|----------------------:|
+# | Investigate physical systems                              |          0.30         |
+# | Construct and document a reproducible process             |          0.10         |
+# | Use analytical, computational, and graphical approaches   |          0.30         |
+# | Provide evidence of the quality of their work             |          0.10         |
+# | Collaborate effectively                                   |          0.20         |
+# 
+# I will develop specific rubric elements based on your engagement with the in-class materials on the first couple of class periods. I need to observe where we need the most support initially and that will change the rubric expectations.

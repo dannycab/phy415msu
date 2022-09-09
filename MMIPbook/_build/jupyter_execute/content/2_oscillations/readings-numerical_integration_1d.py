14dca9bb1756bdf82e494a9409c95fcbfac6b5e0
Danny Caballero <danny.caballero@gmail.com>
2022-09-09 16:19:23 -0400

    updated

--- MMIPbook/_build/jupyter_execute/content/2_oscillations/readings-numerical_integration_1d.py
+++ MMIPbook/_build/jupyter_execute/content/2_oscillations/readings-numerical_integration_1d.py
@@ -3,18 +3,18 @@
 
 # # 2 - Numerical Integration in 1D
 # 
-# Principal Ideas come from [Newman Chapter 8, Section 1]
+# Principal Ideas for this unit will come from [Newman Chapter 8, Section 1](https://github.com/dannycab/phy415msu/blob/main/MMIPbook/assets/pdfs/scans/Newman_Ch8_ODEs.pdf), which is the reading assigned below.
 # 
 # 
-# We have seen how we can quickly exhaust our analytical tools when we investigate systems with interesting and complex behavior. We can see much from the perspective of phase space in terms of the qualitatively different behaviors (e.g., oscillations vs rotations, both clockwise and counter-clockwise, in the large angle pendulum). But what if we can to find the trajectory of a given system.
+# We have seen how we can quickly exhaust our analytical tools when we investigate systems with interesting and complex behavior. We can see much from the perspective of phase space in terms of the qualitatively different behaviors (e.g., oscillations vs rotations, both clockwise and counter-clockwise, in the large angle pendulum). But what if we want to find the specific trajectory of a given set of initial conditions.
 # 
-# That is how can we take a general differential equation:
+# That is we want to be able to take a more general differential equation:
 # 
 # $$\ddot{x} = f(x,\dot{x},t)$$
 # 
-# to $x(t)$? in (most) cases...because sometimes integrals don't converge.
+# to a solution for all (or some specified amount)of time, $x(t)$...in (most) cases...because sometimes integrals don't converge.
 # 
-# We can use **numerical integration** to do so. Below is a video describing 3 coupled ODEs, the Lorenz equations, that are quite famous. You will be able to model systems like this, but for now think of it as motivation for why we want to learn numerical integration.
+# We can use **numerical integration** to find these trajectories. Below is a video describing 3 coupled ODEs, the [Lorenz equations](https://en.wikipedia.org/wiki/Lorenz_system), that are quite famous. You will be able to model systems like this, but for now think of it as motivation for why we want to learn numerical integration.
 
 # In[1]:
 
@@ -23,9 +23,15 @@ from IPython.display import YouTubeVideo
 YouTubeVideo("aAJkLh76QnM", 800, 600)
 
 
-# ## Nonlinear Dynamics and Chaos
+# ## Nonlinear Science
 # 
-# Some of the most interesting problems to work on are 
+# Some of the most interesting ODEs to work with are those that lead to chaos, have interesting behaviors in different parts of phase space, are highly sensitive to initial conditions, or that have complex bound orbits (in real or phase space). This area of study in physics overlaps with a broad interdisciplinary field called [Nonlinear Science](https://en.wikipedia.org/wiki/Nonlinear_system), which studies all manner of systems where the results are not simply proportional to the input features. This work includes theory, applied science, and experimental work. As it is difficult to find truly linear systems in the real world, nonlinear science is quite interdisciplinary with physics research in the areas of [magnetohydrodynamics](https://complex.umd.edu/research/MHD_dynamos/MHD_dynamos.php), [fluid mechanics](), [bio-inspired design](https://meche.mit.edu/people/faculty/PEKO@MIT.EDU), [biophysics (cellular)](https://curtisresearch.gatech.edu/index.php), [biophysics (animal locomotion)](https://crablab.gatech.edu/), [atomic and molecular physics](https://jila.colorado.edu/lewandowski), and many more fields.
+# 
+# We will focus on integrating 1 dimensional ODEs (that is, ODEs of only one variable like the SHO). To that end the reding this week aims to inform you with the simplest of the integrators the [Euler-Cromer integration](https://en.wikipedia.org/wiki/Semi-implicit_Euler_method) technique. This approach is intuitive, straight-forward, and forms the basis for better and far more accurate methods like [Runge-Kutta](https://en.wikipedia.org/wiki/Runge%E2%80%93Kutta_methods). 
+# 
+# Through this week's activities, we will introduce several integration methods, but you will learn to use the [built-in integrator](https://docs.scipy.org/doc/scipy/reference/generated/scipy.integrate.odeint.html?highlight=odeint) (```odeint```) from scipy, which are more efficient than what we could write ourselves.
+# 
+# 
 # 
 
 # ## Book Readings
@@ -43,3 +49,5 @@ YouTubeVideo("aAJkLh76QnM", 800, 600)
 # The second one in from [Chapters 4 and 5 of Steven Strogatz's book, Nonlinear Dynamics and Chaos](https://github.com/dannycab/phy415msu/blob/main/MMIPbook/assets/pdfs/scans/Strogatz_Integrating_Ch4-5.pdf), which is an excellent and broad book covering many science and engineering problems. It is a book that I think every student of physics should read at some point.
 # 
 # **Read This** Section 5.1. We have worked already with these ideas, but we will continue to do so. This is another way to look at what we've done, so it's worth reading another perspective. We will continue to discuss and work with these ideas. You are free to read other parts of this book, but please read 5.1 for class.
+
+# 
--- MMIPbook/_build/jupyter_execute/content/intro.py
+++ MMIPbook/_build/jupyter_execute/content/intro.py
@@ -10,21 +10,12 @@
 # `````{admonition} Updates
 # :class: tip
 # 
+# **09 Sept 22**
+# - [Readings](https://dannycaballero.info/phy415msu/content/2_oscillations/readings-numerical_integration_1d.html) for next week are posted (this is week 2)
+# 
 # **06 Sept 22**
 # - [Class notes](./2_oscillations/notes-SHO_and_dynamical_systems.md) have been posted along with some scans of book sections
 # - A [preliminary rubric](./0_course/rubric.md) for the first project is posted. A detailed breakdown is coming before next class.
-# 
-# **05 Sept 22**
-# - Tuesday's [in-class activity](https://dannycaballero.info/phy415msu/content/2_oscillations/activity-SHO_and_dynamical_systems.html) has been posted.
-#     - Review as you see fit; we will go over the whole thing together
-# 
-# **02 Sept 22**
-# 
-# - [Complete the survey](https://www.when2meet.com/?16627934-Bt7PQ) by Tuesday morning to help us schedule the Project Help Sessions
-# - Synthesized notes from 01 Sept 22 class are [posted](./1_modeling/notes-what_is_a_model.md)
-# - Readings have been updated for next week: [Read Oscillations](./2_oscillations/readings-oscillators.ipynb)
-# - Make sure to complete the [Reading Questions Form](./0_course/reading_questions.md) before class Tuesday morning
-# - More project details are coming, a draft project rubric is forthcoming, and activities for class this coming week will be posted by Monday
 # 
 # `````
 

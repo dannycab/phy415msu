e385ddd9b396d67b3b544932a5c6b1a3217358e5
Danny Caballero <danny.caballero@gmail.com>
2022-10-05 22:04:47 -0400

    added notes

--- MMIPbook/_build/jupyter_execute/content/2_oscillations/activity-Lagrange-with-constraints-analysis.py
+++ MMIPbook/_build/jupyter_execute/content/2_oscillations/activity-Lagrange-with-constraints-analysis.py
@@ -28,33 +28,17 @@
 # 
 # Now we need to write this as a set of first order differential equations. Let $v=\dot{r}$ and $\omega=\dot{\theta}$. Then we get the following four equations
 
-# $$\dot{v} = \left(r^2\omega-g\cot\alpha\right)\cos^2\alpha$$
-# 
 # $$\dot{r} = v$$
 # 
-# $$\dot{\omega} = -2\dfrac{v\omega}{r}$$
+# $$\dot{v} = \left(r^2\omega-g\cot\alpha\right)\cos^2\alpha$$
 # 
 # $$\dot{\theta} = \omega$$
-
-# ## Start your analysis
 # 
-# Now we have four 1st order, coupled, nonlinear differential equations that describe the motion of this bead. We are going to solve for $r(t)$ and $\theta(t)$ for a given choice of $\alpha$. This will produce a solution for $z(t) = \dfrac{r(t)}{\tan\alpha}$. We can investigate phase space, but you will have to look at slices of it and try to make sense of different projections. Let's start with the doing the following three tasks:
-# 
-# We will construct phase space diagrams for $r$ and $\theta$.
-# 
-# 1. You will have to use numpy.meshgrid for four variables. If you put in four variables, then you will get out ordered groups of the four variables.
-# 2. You need to define a function that returns the four changes of the four variables (remember we had to take the ordered pairs and then compute the arrows)
-# 3. Plot a 2d slice (e.g., $r$ vs $v$) 
-
-# In[1]:
-
-
-## Your code here
-
+# $$\dot{\omega} = -2\dfrac{v\omega}{r}$$
 
 # ## Numerically Integrate our EOM
 # 
-# Ok, now that you have looked at the phase space as discuss possible motion that could occur. Let's pick some initial conditions and build a numerical model. 
+# Now we have four 1st order, coupled, nonlinear differential equations that describe the motion of this bead. We are going to solve for $r(t)$ and $\theta(t)$ for a given choice of $\alpha$. This will produce a solution for $z(t) = \dfrac{r(t)}{\tan\alpha}$.  Let's pick some initial conditions and build a numerical model. 
 # 
 # Using the integrator of your choice, numerically integrate and plot (in 3d) the motion of the bead for the following conditions:
 # 
@@ -62,6 +46,36 @@
 # 2. Bead starts at a given height and is given a low speed (less than needed to orbit)
 # 3. Bead starts at a given height and is given a low speed (more than needed to orbit)
 # 4. Can you find a flat horizontal orbit?
+# 
+# We have written the code below so that you could use odeint if you choose.
+# 
+# The goals for this work is to:
+# 
+# 1. Integrate the equations of motion numerically.
+# 2. Plot the motion in various ways (2D slices and 3D space)
+# 
+# We can then use those elements to extract some physics. 
+
+# In[1]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+from scipy.integrate import odeint
+
+def BeadStuckOnCone(x, t, g=9.8, alpha=np.pi/3):
+    
+    r, v, theta, omega = x
+    dxdt = [v, (r**2*omega-g/np.tan(alpha))*np.cos(alpha)**2, omega, -2*v*omega/r]
+    return dxdt
+
+g = 9.8
+alpha = np.pi/3
+
+y0 = [10,0,0,0.0565]  
+t = np.linspace(0,100,1001)
+
 
 # In[2]:
 

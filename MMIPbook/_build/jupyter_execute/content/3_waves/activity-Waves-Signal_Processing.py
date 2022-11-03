74fc7844c603b8e504eeb279de762d4208eb426d
dannycab <danny.caballero@pm.me>
2022-11-03 09:40:26 -0400

    added today's FFT activity

--- MMIPbook/_build/jupyter_execute/content/3_waves/activity-Waves-Signal_Processing.py
+++ MMIPbook/_build/jupyter_execute/content/3_waves/activity-Waves-Signal_Processing.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# # 03 Nov 2022 - Signal Processing
+# 
+# One of the major activities in physics and astronomy that consumes much of our working hours is processing signals. Like we discussed, most instruments and tools give us a $V(t)$ over some finite perioid of time. How do we extract information from the signal and make conclusions about that results? One of the most useful tools for extracting information from time series data (data where there's some temporal aspect to it) is the Fast Fourier Transform (FFT). We introduced it a bit last class, but this time we will systematically investigate its usage and what we try to interpret from the results. We will also do some filtering and signal recovery (inverse FFT).
+# 
+# The specifics of how an FFT works and how it is different from a Fourier Series analysis is important. We will touch on this after you have more experience with what it does. 
+
 # In[1]:
 
 
@@ -11,33 +17,71 @@ import scipy.signal as sig
 get_ipython().run_line_magic('matplotlib', 'inline')
 
 
+# ## Starting the FFT
+# 
+# We will do the first FFT in the code below for a single frequency sinusoidal function. In this example, we know what the FFT should produce (a single peak at the given $w_A$).
+# 
+# $V(t) = A\sin(\omega_At) = A\sin(2\pi f_A t)$
+# 
+# Here we program the precisely known longest period $T_0 = \frac{1}{f_A}$.
+
 # In[2]:
 
 
+## Parameters and setup
+A = 5
+fA = 50
+
+T0 = 1/fA
 N = 1000
-T = 1
-dt = T/N
-tf = T
-VO = 10
+dt = T0/N
 
-t = np.linspace(0,tf,N)
-V = VO*(sig.sawtooth(2*np.pi*t/T)+1)
+## Create time series data (t and V)
+t = np.linspace(0,T0,N)
+V = A*np.cos(2*np.pi*fA*t)
 
-plt.plot(t,V)
+## Plot signal
+fig1 = plt.figure(figsize=(8,6))
+plt.plot(t,V, label='Signal')
+plt.grid()
+plt.xlabel('Time (s)')
+plt.ylabel('Signal (V)')
 
+## Perform FFT and store the result (notice the normalization!)
+Vf = (2/(N*A))*fft(V)
+freq = fftfreq(N,dt)
+
+fig2 = plt.figure(figsize=(8,6))
+## Plottting the results and zooming in (notice we are plotting only positive frequencies)
+plt.scatter(freq[0:N//2], np.abs(Vf[0:N//2]), label='FFT')
+plt.xlabel('Frequency (Hz)')
+plt.ylabel('Intensity (A.U.)')
+plt.grid()
+plt.axis([0,300,-0.1,1.1])
+
+
+# ### Questions
+# 
+# Here's some investigations to perform with the code above:
+# 
+# 1. Change the frequency and amplitudes. Do things change?
+# 2. Change $T_0$ to be some integer value of the current period (e.g., 2$T_0$). What happens?
+# 3. Change $T_0$ to be some non-integer value of the current period (e.g., 3.5$T_0$). What happens?
+# 4. Add more data points. Does anything change?
+# 5. What happens if you mess up the normalization `2/(N*A))*fft(V)`? How can we determine this is the correct normalization?
+# 6. Add a phase to the signal. What happens to the FFT?
+# 7. Add a DC offset (a constant value). What happens to the FFT?
+
+# ## Multi-Frequency signal
+# 
+# Let's unpack what happens when there's multiple frequencies in the signal. Consider some linear combination that looks like:
+# 
+# $$V(t) = \sum_{i=1}^{N} A_n\sin(n \omega_0 t) = \sum_{i=1}^{N} A_n \sin( 2 n \pi f_0 t)$$
+# 
+# Let's see what the FFT looks like.
+# 
 
 # In[3]:
-
-
-Vf = (1/(N*VO))*fft(V)[0:N//2]
-freq = fftfreq(N,dt)[0:N//2]
-
-plt.scatter(freq, np.abs(Vf))
-plt.axis([0,10,-0.1,1.1])
-plt.grid()
-
-
-# In[4]:
 
 
 A = 1
@@ -45,7 +89,7 @@ B = 0.1
 C = -0.2
 
 fA = 50
-fB = 80
+fB = 100
 fC = 200
 
 T0 = 1/fA
@@ -55,217 +99,127 @@ dt = T0/N
 t = np.linspace(0,T0,N)
 V = A*np.cos(2*np.pi*fA*t)+B*np.sin(2*np.pi*fB*t)+C*np.sin(2*np.pi*fC*t)
 
-plt.plot(t,V)
+## Plot signal
+fig1 = plt.figure(figsize=(8,6))
+plt.plot(t,V, label='Signal')
+plt.grid()
+plt.xlabel('Time (s)')
+plt.ylabel('Signal (V)')
 
-
-# In[5]:
-
-
-Vf = (1/(N*VO))*fft(V)
+## Perform FFT and store the result (notice the normalization!)
+Vf = (2/(N*A))*fft(V)
 freq = fftfreq(N,dt)
 
-plt.scatter(freq[0:N//2], np.abs(Vf[0:N//2]))
+fig2 = plt.figure(figsize=(8,6))
+## Plottting the results and zooming in (notice we are plotting only positive frequencies)
+plt.scatter(freq[0:N//2], np.abs(Vf[0:N//2]), label='FFT')
 plt.grid()
-plt.axis([0,300,0,0.1])
+plt.xlabel('Frequency (Hz)')
+plt.ylabel('Intensity (A.U.)')
+plt.axis([0,300,-0.1,1.1])
 
 
-# In[6]:
+# ### Questions
+# 
+# Here's some investigations to perform with the code above:
+# 
+# 1. Change the frequency and amplitudes. How do things change?
+# 2. Change $T_0$ to be some integer value of the current period (e.g., 2$T_0$). What happens?
+# 3. Change $T_0$ to be some non-integer value of the current period (e.g., 3.5$T_0$). What happens?
+# 4. Add more data points. Does anything change?
+# 5. What happens if you mess up the normalization `2/(N*A))*fft(V)`? How can we determine this is the correct normalization?
+# 6. Add a phase to the signal. What happens to the FFT?
+#     1. Do this for each one and then together. Does anything change?
+# 7. Add a DC offset (a constant value). What happens to the FFT?
+
+# ## Noise
+# 
+# Let's add some noise to our signal. We never get anything clean from our instruments, sometimes we don't get a damn thing. But electronic noise, pickup noise, instrument noise, and other sources plague our search for a clear data set. Let's look at noise itself first. SciPy offers us a whole library of random things. Let's just use Gaussian noise.
+# 
+# Gaussian noise is drawn from a random Gaussian distribution. You have two parameters with this kind of noise: $\mu$ and $\sigma$. These are where the center of Gaussian is located ($\mu$) and how spread out it is $\sigma$. The spread is in units of standard deviation, so $\sigma = 1$ is a lot of noise. Below we use "zero centered" noise, $\mu=0$ and plot it. We perform an FFT to see what we get.
+
+# In[4]:
 
 
-VApprox = ifft(N*VO*Vf)
-
-plt.plot(t,VApprox, 'x', label='IFFT')
-plt.plot(t,V, label='original')
-plt.legend()
-
-
-# In[7]:
-
-
-A = 1
-B = 0.1
-C = -0.2
+T0 = 1
+N = 1000
+dt = T0/N
 
 ## Zero centered noise
 mu = 0
 sigma = 0.1
 
-fA = 50
-fB = 80
-fC = 200
-
-T0 = 1/fA
-N = 1000
-dt = T0/N
-
 t = np.linspace(0,T0,N)
-Vdusty = A*np.cos(2*np.pi*fA*t)+B*np.sin(2*np.pi*fB*t)+C*np.sin(2*np.pi*fC*t)+np.random.normal(mu,sigma,len(t))
+V = np.random.normal(mu,sigma,len(t))
 
-plt.plot(t,Vdusty)
+## Plot signal
+fig1 = plt.figure(figsize=(8,6))
+plt.plot(t,V, label='Signal')
+plt.grid()
+plt.xlabel('Time (s)')
+plt.ylabel('Signal (V)')
 
-
-# In[8]:
-
-
-Vdustyf = (1/(N*VO))*fft(Vdusty)
+## Perform FFT and store the result (notice the normalization!)
+Vf = (2/(N*A))*fft(V)
 freq = fftfreq(N,dt)
 
-plt.scatter(freq[0:N//2], np.abs(Vdustyf[0:N//2]))
+fig2 = plt.figure(figsize=(8,6))
+## Plottting the results and zooming in (notice we are plotting only positive frequencies)
+plt.scatter(freq[0:N//2], np.abs(Vf[0:N//2]), label='FFT')
 plt.grid()
-#plt.axis([0,300,0,0.1])
+plt.xlabel('Frequency (Hz)')
+plt.ylabel('Intensity (A.U.)')
+plt.axis([0,300,-0.1,1.1])
 
 
-# In[9]:
+# ### Questions
+# 
+# Here's some investigations to perform with the code above:
+# 
+# 1. Change the $\mu$ and $sigma$. How do things change?
+# 2. Change $T_0$ to be some integer value of the current period (e.g., 2$T_0$). What happens?
+# 3. Change $T_0$ to be some non-integer value of the current period (e.g., 3.5$T_0$). What happens?
+# 4. Add more data points. Does anything change?
+# 5. What happens if you mess up the normalization `2/(N*A))*fft(V)`? How can we determine this is the correct normalization?
+# 6. Overall, what do you notice about the FFT of noise?
 
+# ## Add noise to your single frequency signal
+# 
+# Ok, now you looked a single frequencies, multiple frequencies (albeit integer values), and noise.  
+# 
+# 1. Create a single frequency signal and add noise to it. 
+# 2. Perform the FFT.
+# 3. Investigate it systematically. How does the noise affect the FFT? Can we think about removing the noise? Can you come uo with a scheme to do so?
 
-plt.scatter(freq[0:N//2], np.abs(Vdustyf[0:N//2]))
-plt.grid()
-plt.axis([0,1000,0,0.1])
+# In[5]:
 
 
-# In[10]:
+## your code here
 
 
-plt.scatter(freq[0:N//2], np.abs(Vdustyf[0:N//2]))
-plt.grid()
-plt.axis([0,1000,0,0.01])
+# ## Add noise to your multi-frequency signal
+# 
+# 
+# 1. Create a multi-frequency signal (integer $w_0$) and add noise to it. 
+# 2. Perform the FFT.
+# 3. Investigate it systematically. How does the noise affect the FFT? Can we think about removing the noise? Can you come uo with a scheme to do so?
 
+# In[6]:
 
-# In[11]:
 
+## your code here
 
-## Kill off anything above 400Hz
 
-print(freq[8])
-VCleanf = Vdustyf
-VCleanf[8:] = 0
+# ## Any signal + noise
+# 
+# 1. Create a signal  of your choosing (not sinusoial) and add noise to it (later is fine). 
+# 2. Perform the FFT.
+# 3. Investigate it systematically. How does the noise affect the FFT? Can we think about removing the noise? Can you come uo with a scheme to do so?
 
-VClean = ifft(2*N*VO*VCleanf)
+# In[7]:
 
-plt.plot(t,Vdusty, label='dusty')
-plt.plot(t,VClean, label='cleaned')
-plt.legend()
 
-
-# In[12]:
-
-
-A = 1
-B = 0.1
-C = -0.2
-
-## Zero centered noise
-mu = 0
-sigma = 0.4
-
-fA = 50
-fB = 80
-fC = 200
-
-T0 = 1/fA
-N = 1000
-dt = T0/N
-
-t = np.linspace(0,T0,N)
-Vdusty = A*np.cos(2*np.pi*fA*t)+B*np.sin(2*np.pi*fB*t)+C*np.sin(2*np.pi*fC*t)+np.random.normal(mu,sigma,len(t))
-
-plt.plot(t,Vdusty)
-
-
-# In[13]:
-
-
-Vdustyf = (1/(N*VO))*fft(Vdusty)
-freq = fftfreq(N,dt)
-
-plt.scatter(freq[0:N//2], np.abs(Vdustyf[0:N//2]))
-plt.grid()
-
-
-# In[14]:
-
-
-plt.scatter(freq[0:N//2], np.abs(Vdustyf[0:N//2]))
-plt.grid()
-plt.axis([0,2000,0,0.1])
-
-
-# In[15]:
-
-
-plt.scatter(freq[0:N//2], np.abs(Vdustyf[0:N//2]))
-plt.grid()
-plt.axis([0,2000,0,0.01])
-
-
-# In[16]:
-
-
-## Kill off anything above 400Hz
-
-print(freq[8])
-VCleanf = Vdustyf
-VCleanf[8:] = 0
-
-VClean = ifft(2*N*VO*VCleanf)
-
-plt.plot(t,Vdusty, label='dusty')
-plt.plot(t,VClean, label='cleaned')
-plt.legend()
-
-
-# In[17]:
-
-
-A = 1
-B = 0.1
-C = -0.2
-
-fA = 50
-fB = 80
-fC = 200
-
-phiA = 0
-phiB = 0
-phiC = np.pi/3
-
-T0 = 1/fA
-N = 1000
-dt = T0/N
-
-t = np.linspace(0,T0,N)
-Voriginal = A*np.cos(2*np.pi*fA*t)+B*np.sin(2*np.pi*fB*t)+C*np.sin(2*np.pi*fC*t)
-Vshifted = A*np.cos(2*np.pi*fA*t+phiA)+B*np.sin(2*np.pi*fB*t+phiB)+C*np.sin(2*np.pi*fC*t+phiC)
-
-plt.plot(t,Voriginal, label='Original')
-plt.plot(t,Vshifted, label='w./ Phase')
-plt.legend()
-
-
-# In[18]:
-
-
-Voriginalf = (1/(N*VO))*fft(Voriginal)
-Vshiftedf = (1/(N*VO))*fft(Vshifted)
-freq = fftfreq(N,dt)
-
-plt.plot(freq[0:N//2],np.abs(Voriginalf[0:N//2]))
-plt.plot(freq[0:N//2],np.abs(Vshiftedf[0:N//2]))
-plt.axis([0, 1000, 0, 0.1])
-
-
-# In[19]:
-
-
-VOG = ifft(N*VO*Voriginalf)
-VSH = ifft(N*VO*Vshiftedf)
-
-plt.plot(t,Voriginal, label='Original')
-plt.plot(t,VOG+0.1, label='Original (IFFT)')
-plt.plot(t,Vshifted+2, label='w./ Phase')
-plt.plot(t,VSH+2.1, label='w./ Phase (IFFT)')
-
-plt.legend()
+## your code here
 
 
 # In[ ]:

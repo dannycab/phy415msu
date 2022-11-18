72a2241f2ead2140895e99cdddc473a8c27bfee1
dannycab <danny.caballero@pm.me>
2022-11-18 18:37:19 -0500

    tested on venture

--- MMIPbook/_build/jupyter_execute/content/3_waves/activity-Waves-Cleaning_Signals.py
+++ MMIPbook/_build/jupyter_execute/content/3_waves/activity-Waves-Cleaning_Signals.py
@@ -180,7 +180,7 @@ playsound('data/D_PowerChords_95_SP.wav')
 
 # Let's read the file into an array and plot it. Notice there are two channels.
 
-# In[16]:
+# In[10]:
 
 
 from scipy.io import wavfile
@@ -191,7 +191,7 @@ length = data.shape[0] / samplerate
 print(f"length = {length}s")
 
 
-# In[17]:
+# In[11]:
 
 
 time = np.linspace(0., length, data.shape[0])

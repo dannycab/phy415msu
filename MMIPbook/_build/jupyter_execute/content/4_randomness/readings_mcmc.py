77fc3e276c3822440a2a05143a9314a1631a5602
dannycab <danny.caballero@pm.me>
2022-11-27 10:31:16 -0500

    added notes

---
+++ MMIPbook/_build/jupyter_execute/content/4_randomness/readings_mcmc.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # 29 Nov 2022 - Markov Chain Monte Carlo Modeling
+# 
+# One of the principal tools we have at our disposal when it comes to systems with randomness is the Monte Carlo simulation. Monte Carlo is a large class of approaches that all rely on probabilistic outcomes to determine results. In physics, we often use Monte Carlo to find integrals or sums that we are unable to compute by hand. 
+# 
+# ## Markov Chain Monte Carlo
+# 
+# We often find ourselves using a type of Monte Carlo simulation that makes use of a Markov Chain. This is called the MCMC model. The mathematical foundations of Markov Chains are well beyond the scope of this course, but the principal issue is that we want to describe is how this simulation works. The video below describes the conceptual backing of MCMC and how it was first used in physics in 1953 to develop a model of an ideal gas. We will make a similar model!
+
+# In[1]:
+
+
+from IPython.display import YouTubeVideo
+YouTubeVideo("12eZWG0Z5gY", 800, 600)
+
+
+# In[ ]:
+
+
+
+
